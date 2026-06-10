9da439e1bf312107504a710d5aca13e47cf601c0
Razia Munyua <wambuirazia004@gmail.com>
2026-06-10 16:25:43 +0300

    made changes

--- CashRegister.py
+++ CashRegister.py
@@ -1,65 +1,49 @@
 class CashRegister:
     def __init__(self, discount=0):
         self.discount = discount
-        self.total = 0
+        self.total = 0.0
         self.items = []
         self.previous_transactions = []
 
-    @property
-    def discount(self):
-        return self._discount
+    
 
-    @discount.setter
-    def discount(self, value):
-        if  isinstance(value, int) and 0 <= value <= 100:
-            
-            self._discount = value
-        else:
-            print("Not valid discount")
-            self._discount = 0
-
-    def add_item(self, item, price, quantity): 
+    def add_item(self, title, price, quantity=1): 
         self.total += price * quantity
         
-         
-        self.items.append(
+        for _ in range(quantity):
+            self.items.append(title) 
+        self.previous_transactions.append(
             {
-                "item": item,
+                "title": title,
                 "price": price,
                 "quantity": quantity
             }
          )
-        self.previous_transactions.append(
-            {
-                "item": item,
-                "price": price,
-                "quantity": quantity
-            }
-        )
+        
         
     def apply_discount(self):
-        if len(self.previous_transactions) == 0:
+        if self.discount == 0:
             print("There is no discount to apply.")
             return
+        
         discount_amount = self.total * (self.discount / 100)
         self.total -= discount_amount
-
-        self.previous_transactions.pop()
-
-        if len(self.items) > 0:
-            self.items.pop()
+        print(f"After the discount, the total comes to ${self.total}")
+        
 
     def void_last_transaction(self):
         if len(self.previous_transactions) == 0:
-            print("There is no transaction to void.")
+            
             return
         
         last = self.previous_transactions.pop()
 
-        self.total -= last["price"] * last["quantity"]
+        amount = last["price"] * last["quantity"]
+        self.total -= amount
 
-        for i in range(len(self.items)-1, -1, -1):
-            if self.items[i]["item"] == last["item"] and self.items[i]["price"] == last["price"] and self.items[i]["quantity"] == last["quantity"]:
-                self.items.pop(i)
-                break
-        
+        for _ in range(last["quantity"]):
+            self.items.remove(last["title"])
+        if self.total < 0:
+            self.total = 0.0    
+    def get_items(self):
+        return self.items        
