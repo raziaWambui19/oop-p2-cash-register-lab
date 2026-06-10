95ec766314fb45e3c6b370f317c63adecf037387
Razia Munyua <wambuirazia004@gmail.com>
2026-06-10 15:41:42 +0300

    made changes

--- CashRegister.py
+++ CashRegister.py
@@ -1,10 +1,9 @@
 class CashRegister:
     def __init__(self, discount=0):
         self.discount = discount
-        self.total = 0
+        self.total = 0.0
         self.items = []
         self.previous_transactions = []
-        
     @property 
     def discount(self):
         return self._discount
@@ -17,7 +16,7 @@ class CashRegister:
         else:
             self._discount = value
 
-    def add_item(self, item, price, quantity): 
+    def add_item(self, item, price, quantity=1): 
         self.total += price * quantity
         self.items.extend([item] * quantity)
          
@@ -30,12 +29,12 @@ class CashRegister:
          )
         
     def apply_discount(self):
-        if not self.previous_transactions:
+        if self.discount == 0:
             print("There is no discount to apply.")
             return
-        discount_amount = self.total * (self.discount / 100)
-        self.total -= discount_amount
-            
+        self.total -= self.total * (self.discount / 100)
+        print(f"After the discount, the total comes to ${self.total:.2f}")
+
     def void_last_transaction(self):
         if not self.previous_transactions:
             print("There is no transaction to void.")
@@ -52,3 +51,6 @@ class CashRegister:
         for _ in range(quantity):
             if item in self.items:
                 self.items.remove(item)
+
+        if self.total <= 0:
+            self.total = 0.0    
